e63e33f7b985ab2faa391b0411a2b47b3de53831
Nikita Krivtsov <nikikrschannel@gmail.com>
2019-10-15 17:10:35 +0300

    ЛР №3

--- __main__.py
+++
@@ -1,5 +0,0 @@
-# print(circle.Circle(12, "Бежевый"))
-# print(rectangle.Rectangle(12, 9, "Красный"))
-# print(square.Square(11, "Зелёный"))
-
-
---
+++ main.py
@@ -0,0 +1,7 @@
+from lab_python_oop import circle, rectangle, square
+
+print(circle.Circle(12, "Бежевый"))
+print(rectangle.Rectangle(12, 9, "Красный"))
+print(square.Square(11, "Зелёный"))
+
+
